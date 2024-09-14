357b4d765ade098bde7288b09c8f900c3274e77f
Mudit Khurana <khuranamudii@gmail.com>
2024-09-14 13:16:20 +0530

    2 pointer aproch

---
+++ 3-sum.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int> nums={1,4,6,8,10,45};
+    sort(nums.begin(),nums.end());
+    int targete=13;
+    int ans=0;
+    for(int i=0;i<nums.size()-2;i++){
+        int find=targete-nums[i];
+        int l=i+1;
+        int h=nums.size()-1;
+        while(l<h){
+            if(nums[l]+nums[h]==find){
+                ans++;
+                break;
+            }
+            else if(nums[l]+nums[h]>find){
+                h--;
+            }
+            else{
+                l++;
+            }
+        }
+    }
+    cout<<ans;
+
+}
